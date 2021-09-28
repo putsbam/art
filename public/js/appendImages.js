9c3fa18b2f4ffa1b48db7e4840b02e2004fc06db
Bam <48115974+putsbam@users.noreply.github.com>
2021-09-28 16:08:40 -0300

    Update appendImages.js

--- public/js/appendImages.js
+++ public/js/appendImages.js
@@ -1,3 +1,32 @@
+
+// updating to get url(s) array but still thinking abt using the older script
+
+  const linkArray = ['https://pbs.twimg.com/media/FAJQO4ZUUAQLBvO?format=jpg&name=small', 'https://pbs.twimg.com/media/E_Kq_N4WQAQ2lI3?format=jpg&name=4096x4096', 'https://pbs.twimg.com/media/E_KxUm9WEAM_9VT?format=png&name=360x360', 'https://pbs.twimg.com/media/E_Kq_N_X0AABNH_?format=jpg&name=4096x4096', 'https://pbs.twimg.com/media/E8ys2M0XoAQPEi1?format=jpg&name=large', 'https://pbs.twimg.com/media/E8FabESWQAEVnvM?format=png&name=small', 'https://pbs.twimg.com/media/E8Df-aJXMAMv_58?format=png&name=small', 'https://pbs.twimg.com/media/E7K-8aXXEAo1Dgo?format=jpg&name=4096x4096', 'https://pbs.twimg.com/media/E3oqwXCXEAAm9Lf?format=jpg&name=4096x4096', 'https://pbs.twimg.com/media/ExMbrZqUYAwnVt_?format=jpg&name=large', 'https://pbs.twimg.com/media/EwD6HivXEAQL9hE?format=jpg&name=4096x4096', 'https://pbs.twimg.com/media/EvaNlsIXEAAUKz2?format=jpg&name=medium']
+  
+console.log(linkArray.length);
+
+try {
+  
+for(var i = 0; i <= linkArray.length; i++){
+  
+var img = document.createElement("img");
+img.src = linkArray[i]
+  
+  var column1 = document.getElementById("column1");
+  var column2 = document.getElementById("column2");
+  
+  if(i <= linkArray.length/2){
+    column1.appendChild(img)
+  } else column2.appendChild(img);
+  
+}
+  
+} catch(e){
+  
+  alert(`There was an Error, I'm sorry! :(\nPlease report this: ${e}`)
+  throw new Error(e)
+}
+
 // append images from folder /img
 /*var folder = "/public/img/";
 
@@ -37,31 +66,4 @@ fail: function(xhr, textStatus, errorThrown){
     }
 });
 */
-// updating to get url(s) array but still thinking abt using this script later
-
-  const linkArray = ['https://pbs.twimg.com/media/FAJQO4ZUUAQLBvO?format=jpg&name=small', 'https://pbs.twimg.com/media/E_Kq_N4WQAQ2lI3?format=jpg&name=4096x4096', 'https://pbs.twimg.com/media/E_KxUm9WEAM_9VT?format=png&name=360x360', 'https://pbs.twimg.com/media/E_Kq_N_X0AABNH_?format=jpg&name=4096x4096', 'https://pbs.twimg.com/media/E_Kq_N_WUAALMsy?format=jpg&name=4096x4096', 'https://pbs.twimg.com/media/E8ys2M0XoAQPEi1?format=jpg&name=large', 'https://pbs.twimg.com/media/E8FabESWQAEVnvM?format=png&name=small', 'https://pbs.twimg.com/media/E8Df-aJXMAMv_58?format=png&name=small', 'https://pbs.twimg.com/media/E7K-8aXXEAo1Dgo?format=jpg&name=4096x4096', 'https://pbs.twimg.com/media/E3oqwXCXEAAm9Lf?format=jpg&name=4096x4096', 'https://pbs.twimg.com/media/ExMbrZqUYAwnVt_?format=jpg&name=large', 'https://pbs.twimg.com/media/EwD6HivXEAQL9hE?format=jpg&name=4096x4096', 'https://pbs.twimg.com/media/EvaNlsIXEAAUKz2?format=jpg&name=medium']
-  
-  console.log(linkArray.length);
-
-try {
-  
-for(var i = 0; i <= linkArray.length; i++){
-  
-var img = document.createElement("img");
-img.src = linkArray[i]
-  
-  var column1 = document.getElementById("column1");
-  var column2 = document.getElementById("column2");
-  
-  if(i <= linkArray.length/2){
-    column1.appendChild(img)
-  } else column2.appendChild(img);
-  
-}
-  
-} catch(e){
-  
-  alert(`There was an Error, I'm sorry! :(\nPlease report this: ${e}`)
-  throw new Error(e)
-}
  
