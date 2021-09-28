f8111f474966d38c1e83ff7dd7e124de3d09cd51
Bam <48115974+putsbam@users.noreply.github.com>
2021-09-28 18:29:47 -0300

    Update appendImages.js

--- public/js/appendImages.js
+++ public/js/appendImages.js
@@ -1,7 +1,7 @@
 
 // updating to get url(s) array but still thinking abt using the older script
 
-  const linkArray = ['https://pbs.twimg.com/media/FAJQO4ZUUAQLBvO?format=jpg&name=small']
+const linkArray = ['https://pbs.twimg.com/media/FAJQO4ZUUAQLBvO?format=jpg&name=small', 'https://pbs.twimg.com/media/E_Kq_N4WQAQ2lI3?format=jpg&name=4096x4096', 'https://pbs.twimg.com/media/E_KxUm9WEAM_9VT?format=png&name=360x360', 'https://pbs.twimg.com/media/E_Kq_N_X0AABNH_?format=jpg&name=4096x4096', 'https://pbs.twimg.com/media/E_Kq_N_WUAALMsy?format=jpg&name=4096x4096', 'https://pbs.twimg.com/media/E8ys2M0XoAQPEi1?format=jpg&name=large', 'https://pbs.twimg.com/media/E8FabESWQAEVnvM?format=png&name=small', 'https://pbs.twimg.com/media/E8Df-aJXMAMv_58?format=png&name=small', 'https://pbs.twimg.com/media/E7K-8aXXEAo1Dgo?format=jpg&name=4096x4096', 'https://pbs.twimg.com/media/E3oqwXCXEAAm9Lf?format=jpg&name=4096x4096', 'https://pbs.twimg.com/media/ExMbrZqUYAwnVt_?format=jpg&name=large', 'https://pbs.twimg.com/media/EwD6HivXEAQL9hE?format=jpg&name=4096x4096', 'https://pbs.twimg.com/media/EvaNlsIXEAAUKz2?format=jpg&name=medium']
   
 console.log(linkArray.length);
 
